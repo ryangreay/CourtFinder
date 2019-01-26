2472268ec2431ad8dcac3381be65ea7065daf0ad
ryangreay <ryanster505@gmail.com>
2019-01-26 12:29:57 -0800

    Add project files.

---
+++ CourtFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CourtFinder.Startup))]
+namespace CourtFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
